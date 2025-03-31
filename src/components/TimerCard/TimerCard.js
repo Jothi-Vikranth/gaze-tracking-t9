f45bc445a1050250c040941b4c298441f4869a93
Jothi-Vikranth <jothivikranth213@gmail.com>
2025-03-31 16:59:16 +0530

    updated timer component from circle to rectangle

--- src/components/TimerCard/TimerCard.js
+++ src/components/TimerCard/TimerCard.js
@@ -1,15 +1,26 @@
 import { Box } from "@mui/material";
 import React, { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { SPEED } from "../../app/features/settingsSlice";
+
+const SELECTION_SPEED_DELAP_MAP = {
+  [SPEED.SLOW]: 3000,
+  [SPEED.MED]: 2250,
+  [SPEED.FAST]: 1500,
+};
 
 const TimerCard = ({ onTimerClick, children, active = true }) => {
-  const delay = 3000;
-  const length = 180;
+  const selectionSpeed = useSelector(
+    (state) => state.settingsData.selectionSpeed
+  );
+
+  const delay = SELECTION_SPEED_DELAP_MAP[selectionSpeed];
+
+  const width = 160;
+  const height = 120;
   const thickness = 20;
 
-  const circleWidth = Math.floor(length / 2);
-  const circleRadius = Math.floor(circleWidth - thickness / 2);
-
-  const dashArrayCount = Math.floor(3.14 * 2 * circleRadius);
+  const dashArrayCount = (width + height - 4) * 2; // Perimeter of rectangle
 
   const [progress, setProgress] = useState(0);
   const timerRef = useRef(null);
@@ -39,9 +50,9 @@ const TimerCard = ({ onTimerClick, children, active = true }) => {
 
     // Start updating the progress bar
     progressIntervalRef.current = setInterval(() => {
-      elapsed += 50; // Update every 50ms
+      elapsed += 5; // Update every 50ms
       setProgress((elapsed / delay) * 100);
-    }, 50);
+    }, 5);
   };
 
   const resetProgress = () => {
@@ -65,11 +76,11 @@ const TimerCard = ({ onTimerClick, children, active = true }) => {
         sx={{
           position: "relative",
           //   aspectRatio: 1,
-          width: length,
-          height: length,
+          width: width,
+          height: height,
           //   width: `${length}px`,
           //   height: `${length}px`,
-          borderRadius: "50%",
+          borderRadius: 5,
           p: `${thickness}px`,
           //   boxShadow:
           //     "6px 6px 10px -1px rgba(0 0 0 /.15),-6px -6px 10px -1px rgba(255 255 255 / .7)",
@@ -82,9 +93,9 @@ const TimerCard = ({ onTimerClick, children, active = true }) => {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            width: `${length - 2 * thickness}px`,
-            height: `${length - 2 * thickness}px`,
-            borderRadius: "50%",
+            width: `${width - 2 * thickness}px`,
+            height: `${height - 2 * thickness}px`,
+            borderRadius: 5,
             letterSpacing: "1.5px",
             // border: 1,
             backgroundColor: active ? "#ffffe4" : "#801515",
@@ -94,33 +105,41 @@ const TimerCard = ({ onTimerClick, children, active = true }) => {
           {children}
         </Box>
       </Box>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-        width={length}
-        height={length}
+      {/* <svg
+        width="200"
+        height="120"
         style={{ position: "absolute", top: 0, left: 0 }}
       >
-        <defs>
-          <linearGradient id="GradientColor">
-            <stop offset="0%" stop-color="#e91e63" />
-            <stop offset="100%" stop-color="#673ab7" />
-          </linearGradient>
-        </defs>
-        <circle
-          cx={`${circleWidth}`}
-          cy={`${circleWidth}`}
-          r={`${circleRadius}`}
-          stroke-linecap="round"
+        <path
+          d="M 20 20 H 160 V 80 H 20 Z"
+          stroke="#4F6DB6"
+          stroke-width={`${thickness}`}
           fill="none"
-          strokeWidth={`${thickness}`}
-          strokeDasharray={dashArrayCount}
+          stroke-dasharray="400"
+          stroke-dashoffset="200"
+        ></path>
+      </svg> */}
+      <svg
+        width={width}
+        height={height}
+        style={{ position: "absolute", top: 0, left: 0 }}
+      >
+        <rect
+          x={parseInt(thickness / 2)}
+          y={parseInt(thickness / 2)}
+          width={width - thickness}
+          height={height - thickness}
+          // rx={thickness}
+          // ry={thickness}
+          stroke="#4F6DB6"
+          stroke-width={`${thickness}`}
+          fill="none"
+          stroke-dasharray={dashArrayCount}
+          // stroke-dashoffset="0"
           strokeDashoffset={Math.floor(
             dashArrayCount * (1 - parseFloat(progress / 100))
           )}
-          stroke="#4F6DB6"
-          transform={`rotate(-82 90 90)`}
-        />
+        ></rect>
       </svg>
     </Box>
   );
